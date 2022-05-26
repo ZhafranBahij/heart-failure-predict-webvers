d5f455751e933aa7ec9b315099d77fff2cafd679
ZhafranBahij <muhammadzhafranbahij@gmail.com>
2022-05-26 10:46:27 +0700

    Kono Diavolo ga

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ app = Flask(__name__, static_url_path='', static_folder='frontend/dist')
 # CORS(app) #comment this on deployment
 api = Api(app)
 
+@app.route("/", defaults={'path':''})
+def serve(path):
+    return send_from_directory(app.static_folder,'index.html')
+
 """
 Jika tidak ada definisi methods, berarti methods=['GET']
 Agar bisa diterima oleh axios, dikirim dengan jsonify()
